29fa502a9933b157c7f2d28a04283f7dcc85f950
lizikang <13301068@bjtu.edu.cn>
2016-04-12 16:41:43 +0800

    from lizikang

--- yryj/src/com/yryj/daoImpl/TypeDL.java
+++ yryj/src/com/yryj/daoImpl/TypeDL.java
@@ -10,25 +10,40 @@ public class TypeDL extends HibernateDaoSupport implements TypeDao{
 	@Override
 	public void save(Type type) {
 		// TODO Auto-generated method stub
-		
+		getHibernateTemplate().setCheckWriteOperations(false);
+		getHibernateTemplate().save(type);
 	}
 
 	@Override
-	public void delete(Type type) {
+	public void delete(int id) {
 		// TODO Auto-generated method stub
-		
+		Type type=find(id);
+		try{
+			Session session=getHibernateTemplate().getSessionFactory().openSession();
+			Transaction ts=session.beginTransaction();
+			session.delete(type);
+			ts.commit();
+			session.close();
+			session=null;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void update(Type type) {
 		// TODO Auto-generated method stub
-		
+		getHibernateTemplate().update(type);
 	}
 
 	@Override
-	public Type find(Type type) {
+	public Type find(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		List list=getHibernateTemplate().find("from Type where id=?",id);
+		if(list.size()>0)
+			return (Type) list.get(0);
+		else
+			return null;
 	}
 
 }
